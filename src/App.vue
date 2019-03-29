3ed4750328037ee501c45014d8765649c05d7f0f
dev-iskan <successfuliskan@gmail.com>
2019-03-30 00:38:29 +0500

    removed unused component, fixed form styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <upload-form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import UploadForm from '@/components/UploadForm'
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    UploadForm
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  body {
+    font-family: Arial, sans-serif
+  }
 </style>
---
+++ src/components/UploadForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="dragndrop">
+    <input
+      id="file"
+      type="file"
+      name="files[]"
+      multiple
+      class="dragndrop__input"
+    >
+    <label
+      for="file"
+      class="dragndrop__header"
+    >
+      <strong>Drag files here</strong> or click to select files
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .dragndrop {
+    --dragndrop-min-height: 200px;
+    width: 100%;
+    min-height: var(--dragndrop-min-height);
+    background-color: #f8f8f8;
+    position: relative;
+    border: 3px dashed rgba(0,0,0, .2);
+  }
+
+  .dragndrop__input {
+    display: none;
+  }
+
+  .dragndrop__header {
+    display: block;
+    font-size: 1.1em;
+    color: #555;
+    vertical-align: middle;
+    text-align: center;
+    margin: calc(var(--dragndrop-min-height) / 2) 20px;
+  }
+
+  .dragndrop__header:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  .dragndrop__header--compact {
+    margin: calc(var(--dragndrop-min-height) / 4) 20px;
+  }
+</style>
